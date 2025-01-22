de4df5cff81517ed6bb79a5f6939d405f3a5c0c9
AYPG2023 <andersonperomo525@gmail.com>
2025-01-22 12:55:59 -0600

    Avances en el formulario de para publicar una vacante

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,4 +1,74 @@
-<div>
-    {{-- Stop trying to control. --}}
-    <h1>Desde Livewire</h1>
-</div>
+<form class="md:w-1/2 space-y-5"> <!-- Separada todos sus hijos o cada boton -->
+    <!--Boton para vacante-->
+    <div>
+        <x-input-label for="titulo" :value="__('Nombre de la vacante')" />
+        <x-text-input id="titulo"
+            class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+            type="text" name="titulo" :value="old('titulo')" required autofocus autocomplete="titulo"
+            placeholder="Nombre de la vacante" />
+        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
+    </div>
+    <!--Boton para seleccionar salario -->
+
+    <div>
+        <x-input-label for="salario" :value="__('Salario mensual')" />
+        <select name="salario" id="salario"
+            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full">
+        </select>
+        <x-input-error :messages="$errors->get('salario')" class="mt-2" />
+    </div>
+
+    <!--Boton para categoria -->
+
+    <div>
+        <x-input-label for="categoria" :value="__('Categoria')" />
+        <select name="categoria" id="categoria"
+            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full">
+        </select>
+        <x-input-error :messages="$errors->get('categoria')" class="mt-2" />
+    </div>
+
+    <!--Boton para nombre de la empresa-->
+
+    <div>
+        <x-input-label for="empresa" :value="__('Nombre de la empresa')" />
+        <x-text-input id="empresa"
+            class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+            type="text" name="empresa" :value="old('empresa')" required autofocus autocomplete="empresa"
+            placeholder="Nombre de la empresa" />
+        <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
+    </div>
+
+    <!--Boton para caducidad de vacante-->
+
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Fecha de caducidad de postulación')" />
+        <x-text-input id="ultimo_dia"
+            class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+            type="date" name="ultimo_dia" :value="old('ultimo_dia')" required autofocus autocomplete="ultimo_dia" />
+        <x-input-error :messages="$errors->get('ultimo_dia')" class="mt-2" />
+    </div>
+
+    <!--Boton para Descripción del trabajo-->
+
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción del trabajo')" />
+        <textarea id="descripcion"
+            class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full h-72"
+            name="descripcion" placeholder="Descripción del trabajo" required autofocus autocomplete="descripcion">{{ old('descripcion') }}</textarea>
+        <x-input-error :messages="$errors->get('descripcion')" class="mt-2" />
+    </div>
+
+    <!--Boton para imagen-->
+    <div>
+        <x-input-label for="imagen" :value="__('Imagen')" />
+        <x-text-input id="imagen"
+            class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+            type="file" name="imagen" required />
+        <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
+    </div>
+
+    <x-primary-button class="w-full justify-center">
+        {{ __('Publicar vacante') }}
+    </x-primary-button>
+</form>
