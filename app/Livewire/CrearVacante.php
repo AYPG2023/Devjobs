37d1360624c42eec19e73d569166c1427bac0205
AYPG2023 <andersonperomo525@gmail.com>
2025-01-22 17:05:57 -0600

    Avances en el formulario de para publicar una vacante [Creacion de migraciones]

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -2,12 +2,36 @@
 
 namespace App\Livewire;
 
+use App\Models\Categoria;
+use App\Models\Salario;
 use Livewire\Component;
 
 class CrearVacante extends Component
 {
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+
+    protected $rules = [
+        'titulo' => 'required',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required'
+    ];
+
     public function render()
     {
-        return view('livewire.crear-vacante');
+        // Consulta a base de datos
+        $salarios = Salario::all();
+        $categorias = Categoria::all();
+
+        return view('livewire.crear-vacante', [
+            'salarios' => $salarios,
+            'categorias' => $categorias
+        ]);
     }
 }
+ // Path: resources/views/livewire/crear-vacante.blade.php
+ // Vista de la tabla
+ // se le para el resultado de la consulta a la vista, y se le pasa los parametros para que retorne la lista 
--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -4,7 +4,7 @@
         <x-input-label for="titulo" :value="__('Nombre de la vacante')" />
         <x-text-input id="titulo"
             class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
-            type="text" name="titulo" :value="old('titulo')" required autofocus autocomplete="titulo"
+            type="text" wire:model="titulo" :value="old('titulo')" required autofocus autocomplete="titulo"
             placeholder="Nombre de la vacante" />
         <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
     </div>
@@ -12,8 +12,12 @@
 
     <div>
         <x-input-label for="salario" :value="__('Salario mensual')" />
-        <select name="salario" id="salario"
+        <select wire:model="salario" id="salario"
             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full">
+        <option >-- Seleccione --</option>
+        @foreach ($salarios as $salario)
+            <option value="{{ $salario->id }}">{{ $salario->salario }}</option>
+        @endforeach
         </select>
         <x-input-error :messages="$errors->get('salario')" class="mt-2" />
     </div>
@@ -22,8 +26,12 @@
 
     <div>
         <x-input-label for="categoria" :value="__('Categoria')" />
-        <select name="categoria" id="categoria"
+        <select wire:model="categoria" id="categoria"
             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full">
+            <option >-- Seleccione --</option>
+            @foreach ($categorias as $categoria)
+                <option value="{{ $categoria->id }}">{{ $categoria->categoria }}</option>
+            @endforeach
         </select>
         <x-input-error :messages="$errors->get('categoria')" class="mt-2" />
     </div>
@@ -34,7 +42,7 @@
         <x-input-label for="empresa" :value="__('Nombre de la empresa')" />
         <x-text-input id="empresa"
             class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
-            type="text" name="empresa" :value="old('empresa')" required autofocus autocomplete="empresa"
+            type="text" wire:model="empresa" :value="old('empresa')" required autofocus autocomplete="empresa"
             placeholder="Nombre de la empresa" />
         <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
     </div>
@@ -45,7 +53,7 @@
         <x-input-label for="ultimo_dia" :value="__('Fecha de caducidad de postulación')" />
         <x-text-input id="ultimo_dia"
             class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
-            type="date" name="ultimo_dia" :value="old('ultimo_dia')" required autofocus autocomplete="ultimo_dia" />
+            type="date" wire:model="ultimo_dia" :value="old('ultimo_dia')" required autofocus autocomplete="ultimo_dia" />
         <x-input-error :messages="$errors->get('ultimo_dia')" class="mt-2" />
     </div>
 
@@ -55,7 +63,7 @@
         <x-input-label for="descripcion" :value="__('Descripción del trabajo')" />
         <textarea id="descripcion"
             class="block mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full h-72"
-            name="descripcion" placeholder="Descripción del trabajo" required autofocus autocomplete="descripcion">{{ old('descripcion') }}</textarea>
+            wire:model="descripcion" placeholder="Descripción del trabajo" required autofocus autocomplete="descripcion">{{ old('descripcion') }}</textarea>
         <x-input-error :messages="$errors->get('descripcion')" class="mt-2" />
     </div>
 
